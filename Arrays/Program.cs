bfeb00cdc980e0faa5778f6120455a90cd4f1fd8
Vladimir Zotov <zott14@outlook.com>
2018-04-08 17:43:31 +0300

    Add SimpleArray

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,6 +10,23 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            int[] array = new int[10];
+
+            //Заполнение массива.
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+            
+            // Вывод на экран значений элементов массива в обратном порядке.
+            for (int i = array.Length -1; i >= 0; i--) 
+            {
+                Console.Write(array[i]);
+            }
+
+            // Delay
+            Console.ReadKey();
+
         }
     }
 }
