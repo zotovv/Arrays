50f5913ab2ec9c46584a8d79170ba2633c41d500
Vladimir Zotov <zott14@outlook.com>
2018-04-11 20:05:50 +0300

    Update

--- UsersArray/Program.cs
+++ UsersArray/Program.cs
@@ -13,9 +13,9 @@ namespace UsersArray
             Console.WriteLine("Введите число элементов массива: ");
             int[]array  = new int[Convert.ToInt32(Console.ReadLine())];
 
-
             int maxValue = 0, minValue = 0, sum = 0;
 
+            // Заполнение массива
             for (int i = 0; i < array.Length; i++)
             {
                 array[i] = i;
@@ -24,6 +24,8 @@ namespace UsersArray
             maxValue = array[0];
             minValue = array[0];
 
+            // Вывод на консоль нечётных, максимального, минимального,
+            // среднего арифметического и суммы значений массива.
             for (int i = 0; i < array.Length; i++)
             {
                 if ((array[i] % 2) != 0)
@@ -40,9 +42,7 @@ namespace UsersArray
                 {
                     minValue = array[i];
                 }
-
-                
-
+             
                 sum += array[i];
             }
 
