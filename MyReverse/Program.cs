12a87688526b27425b4ba4b27b7d9a3ee5fe5f8a
Vladimir Zotov <zott14@outlook.com>
2018-04-14 12:07:30 +0300

    Add MyReverse and SubArray

---
+++ MyReverse/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyReverse
+{
+    class Program
+    {
+        static int[] MyReverse(int[] array)
+        {
+            
+            int buf = 0;
+            int n = array.Length;
+            int j = n - 1;
+
+            for (int k = 0; k < j; k++)
+            {
+                buf = array[k];
+                array[k] = array[j];
+                array[j] = buf;
+                j--;
+            }
+
+            return array;           
+        }
+           
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = i + 1;
+            }
+
+            array = MyReverse(array);
+
+            for (int i = 0; i < array.Length; i++)
+
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SubArray/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubArray
+{
+    class Program
+    {
+        static int[] SubArray(int[] array, int index, int count)
+        {
+            int[] array1 = new int[count];
+            int k = 0;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array.Length > index)
+                {
+                    array1[k] = array[index];
+                }
+                else
+                {
+                    array1[k] = 1;
+                }
+
+                index++;
+                k++;
+            }
+
+            return array1;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i * 2;
+            }
+
+
+            Console.WriteLine("Введите индекс массива: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите количество элементов: "); 
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            array = SubArray(array, index, count);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+          
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
